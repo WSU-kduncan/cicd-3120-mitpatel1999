0da2eebcac069a17c86dd75e8a3be452f3e0c0f1
mitpatel1999 <77901596+mitpatel1999@users.noreply.github.com>
2022-04-22 12:17:22 -0400

    Update restart.sh

--- webhooks/restart.sh
+++ webhooks/restart.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-docker kill my-apache2
+docker kill webserver
 
 yes | docker system prune
 
 docker pull mitpatelceg3120/patelceg3120:latest
 
-docker run -d --rm --name my-apache2 -p 80:80 mitpatelceg3120/patelceg3120:latest
+docker run -d --rm --name webserver -p 80:80 mitpatelceg3120/patelceg3120:latest
